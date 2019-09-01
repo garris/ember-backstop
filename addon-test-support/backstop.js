456bb7f603b91a82f883793567348746a7d03588
EricD <daisu@NANOFACTORY.COM>
2019-09-01 10:50:09 -0700

    Helper : add auto-increment naming for multiple assertions in a test. Add options for appending a custom name in the assertion.

--- addon-test-support/backstop.js
+++ addon-test-support/backstop.js
@@ -76,20 +76,13 @@ function json(response) {
 }
 
 //I'm in your webapps -- checkin ur screenz.
-function backstopHelper(name, options, res, err) {
-  let testHash = {};
+function backstopHelper(name, testHash, options, res, err) {
   if (!name) {
     throw new Error('Backstop helper requires an unique name or an assert obj.');
   }
 
-  // Automatic name generation for QUnit tests by passing in the `assert` object.
-  if (name.test && name.test.module && name.test.module.name && name.test.testName) {
-    testHash.testId = window._testRunTime;
-    testHash.scenarioId = name.test.testId;
-    name = `${name.test.module.name} | ${name.test.testName}`;
-  } else if (name.fullTitle) {
-    // Automatic name generation for Mocha tests by passing in the `this.test` object.
-    name = name.fullTitle();
+  if (!testHash) {
+    throw new Error("Backstop helper requires an unique id for the testHash.");
   }
 
   let snapshotRoot;
@@ -156,13 +149,51 @@ function backstopHelper(name, options, res, err) {
   }, 0);
 }
 
+function createNameHash(assert, options) {
+  let name = options && options.name ? options.name : ""; //optional name to append to the assertion
+  let assertionName;
+  let testHash = {};
+
+  //namespace our steps to not conflict with qunit steps functionality
+  if (!assert.test.backstop) assert.test.backstop = {};
+  if (!assert.test.backstop.steps) assert.test.backstop.steps = [];
+
+  let steps = assert.test.backstop.steps;
+
+  if (name && name.length !== 0) {
+    name = ` | ${name}`;
+  }
+
+  // Generate base names to extend
+  if (
+    assert.test &&
+    assert.test.module &&
+    assert.test.module.name &&
+    assert.test.testName
+  ) {
+    testHash.testId = window._testRunTime;
+    testHash.scenarioId = assert.test.testId;
+    assertionName = `${assert.test.module.name} | ${assert.test.testName}`;
+  }
+
+  //Record our step
+  steps.push(steps.length);
+
+  // Name generation based on steps and optional name for the step
+  const stepNumber = steps.length - 1;
+  assertionName = `${assertionName}${name} | assert${stepNumber}`;
+
+  return { name: assertionName, testHash: testHash };
+}
+
 /**
  * I'm in your webapps -- checkin your screenz. -schooch
  */
-export default async function(assert, options) {
+export default async function (assert, options) {
+  const hash = createNameHash(assert, options);
   return new Promise((res, err) => {
-    backstopHelper(assert, options, res, err);
-  }).then((backstopResult) => {
+    backstopHelper(hash.name, hash.testHash, options, res, err);
+  }).then(backstopResult => {
     assert.ok(backstopResult.ok, backstopResult.message);
   });
 }
