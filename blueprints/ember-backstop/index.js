71c9ea009e077ec34e9ffdaf2b070fc2ffae0c06
Shankar Sridhar <shankar.utd@gmail.com>
2019-12-19 07:52:54 -0800

    Make backstopJs as a dependency instead of adding it to the consuming app's package.json

--- blueprints/ember-backstop/index.js
+++ blueprints/ember-backstop/index.js
@@ -3,12 +3,5 @@
 module.exports = {
   description: 'Add dependencies for BackstopJS visual regression testing addon.',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
-
-  afterInstall() {
-    return this.addPackagesToProject([
-      {name : 'backstopjs'}
-    ]);
-  }
-
+  normalizeEntityName() {} // no-op since we're just adding dependencies
 };
