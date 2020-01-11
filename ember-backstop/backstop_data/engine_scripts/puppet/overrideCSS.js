d5f173d817fef65b0c2fa404be2551febde53cdf
Shankar Sridhar <shankar.utd@gmail.com>
2020-01-10 16:29:49 -0800

    Reverting some changes to overrideCSS.js

--- ember-backstop/backstop_data/engine_scripts/puppet/overrideCSS.js
+++ ember-backstop/backstop_data/engine_scripts/puppet/overrideCSS.js
@@ -4,10 +4,11 @@ const BACKSTOP_TEST_CSS_OVERRIDE = `#ember-testing {width: 100% !important; heig
 
 module.exports = function(page, scenario) {
   // inject arbitrary css to override styles
+  page.evaluate(`window._styleData = '${BACKSTOP_TEST_CSS_OVERRIDE}'`);
   page.evaluate(() => {
     let style = document.createElement('style');
     style.type = 'text/css';
-    let styleNode = document.createTextNode(`${BACKSTOP_TEST_CSS_OVERRIDE}`);
+    let styleNode = document.createTextNode(window._styleData);
     style.appendChild(styleNode);
     document.head.appendChild(style);
   });
