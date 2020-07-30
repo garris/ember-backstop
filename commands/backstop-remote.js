a7457c1daf8a58d2160a07dfc5147b7ac67bfac4
Shankar Sridhar <shankar.utd@gmail.com>
2020-07-29 20:54:56 -0700

    pass interactive mode command option

--- commands/backstop-remote.js
+++ commands/backstop-remote.js
@@ -3,17 +3,19 @@ const backstopjs = require('backstopjs');
 
 module.exports = {
   name: 'backstop:remote',
+
   aliases: ['backstop-remote'],
+
   availableOptions: [
-    { name: 'filter', type: String, aliases: ['f'], default: ''},
-    { name: 'config', type: String, aliases: ['c'], default: './backstop.js' }
+    { name: 'filter', type: String, aliases: ['f'], default: '' },
+    { name: 'config', type: String, aliases: ['c'], default: './backstop.js' },
+    { name: 'i', type: Boolean },
   ],
+
   description: 'Launch Backstop-Remote server.',
-  run(commandOptions) {
+
+  run({ config, filter, i }) {
     process.chdir('./ember-backstop');
-    return backstopjs('remote', {
-      config: commandOptions.config, 
-      filter: commandOptions.filter
-    });
-  }
+    return backstopjs('remote', { config, filter, i });
+  },
 };
